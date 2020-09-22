dd1bc95dafb5e463c8c0afab03f0855d5cf48654
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-09-21 19:34:21 -0700

    setup database collections

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import {Database} from '../lib/types';
+import { Booking, Database, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
@@ -11,7 +11,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
-    bookings: db.collection("test_bookings")
+    listings: db.collection<Listing>("listings"),
+    bookings: db.collection<Booking>("bookings"),
+    users: db.collection<User>("users"),
   };
 };
