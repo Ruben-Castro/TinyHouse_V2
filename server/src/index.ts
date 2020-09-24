13a23ff079b1f4c3c5929ac6cf6b7ea9d8c7e46a
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-09-23 23:22:25 -0700

    added cookie functionality to the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,17 +5,19 @@ import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
-
+import cookieParser from "cookie-parser";
 const app = express();
 
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({req, res}) => ({ db, req, res }),
   });
   server.applyMiddleware({ app, path: "/api" });
 
