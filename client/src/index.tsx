a5f8d22f0245bc0230286732f19d64da45935fab
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-09-23 22:36:08 -0700

    app Header with logout functionality complete

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import {
+  AppHeader,
   Home,
   Host,
   Listing,
@@ -13,7 +14,7 @@ import {
 } from "./sections";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Layout } from "antd";
+import { Affix, Layout } from "antd";
 import { Viewer } from "./lib/types";
 import "./styles/index.css";
 
@@ -31,10 +32,12 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
-  console.log(viewer);
   return (
     <Router>
       <Layout id="app">
+        <Affix offsetTop={0} className="app__affix-header">
+          <AppHeader viewer={viewer}  setViewer={setViewer}/>
+        </Affix>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/host" component={Host} />
