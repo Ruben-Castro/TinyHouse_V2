60bca4d93889facb0d54557296ad2d6c02b723b9
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-10-02 22:24:15 -0700

    modified typedef and resolver to handle querying for user

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -10,12 +10,63 @@ type Viewer {
   didRequest:Boolean!
 }
 
+type Bookings {
+  total: Int!
+  result: [Booking!]!
+}
+type Listings {
+  total: Int!
+  result:[Listing!]!
+}
+
+type Booking {
+  id:ID!
+  listing: Listing!
+  tenant: User!
+  checkIn: String!
+  checkOut:String!
+
+}
+
+type Listing {
+  id: ID!
+  title:String
+  description:String
+  image:String!
+  host:User!
+  type: ListingType!
+  address:String!
+  city:String!
+  bookings(limit: Int!, page:Int!): Bookings
+  bookingsIndex:String! 
+  price: Int!
+  numOfGuests:Int!
+
+}
+
+enum ListingType {
+  APARTMENT
+  HOUSE
+}
+
+type User {
+  id: ID
+  name: String!
+  avatar: String!
+  contact:String!
+  hasWallet:Boolean!
+  income:Int
+  bookings(limit:Int!, page: Int!):Bookings
+  listings(limit:Int!, page: Int!):Listings!
+}
+
 input LogInInput {
     code:String!
   }
 
   type Query {
     authUrl: String!
+    user(id:ID!): String!
   }
 
  
