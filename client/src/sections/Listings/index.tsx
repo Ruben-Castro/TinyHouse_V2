b42895e9e53fa1824ec7bfcdd45f026cd03bf9e1
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-10-07 15:54:03 -0700

    querying by location complete partial ui for listings complete

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,5 +1,85 @@
 import React from "react";
+import { RouteComponentProps, Link} from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { Layout, List, Typography } from "antd";
+import { ListingCard, PageSkeleton } from "../../lib/components";
+import { LISTINGS } from "../../lib/graphql/queries";
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
 
-export const Listings = () => {
-  return <h2>Listings</h2>;
+interface MatchParams {
+  location: string;
+}
+
+const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
+
+const PAGE_LIMIT = 8;
+
+export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const { data, error, loading } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter: ListingsFilter.PRICE_LOW_TO_HIGH,
+        limit: PAGE_LIMIT,
+        page: 1,
+      },
+    }
+  );
+
+  if (loading) {
+    
+  }
+
+  if (error) {
+  }
+
+  const listings = data ? data.listings : null;
+  const listingsRegion = listings ? listings.region : null;
+
+  const listingsRegionElement = listingsRegion ? (
+    <Title className="listings__title" level={3}>
+      Results for the listing region {listingsRegion}
+    </Title>
+  ) : null;
+
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <List
+        grid={{
+          gutter: 8,
+          xs: 1,
+          sm: 2,
+          lg: 4,
+        }}
+        dataSource={listings.result}
+        renderItem={(listing) => (
+          <List.Item>
+            <ListingCard listing={listing} />
+          </List.Item>
+        )}
+      />
+    ) : (
+      <div>
+        <Paragraph>
+          No listings have been created for{" "}
+          <Text mark> "{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          Be the first person to creat a <Link to="/host">listing in this area</Link>!
+        </Paragraph>
+      </div>
+    );
+
+  return (
+    <Content className="listings">
+      {listingsRegionElement}
+      {listingsSectionElement}
+    </Content>
+  );
 };
