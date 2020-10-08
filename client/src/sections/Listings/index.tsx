1716fe39224347ceeae71b47376ede6c9261d6f1
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-10-07 17:48:43 -0700

    frontend for listings complete with minor layout problems

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { RouteComponentProps, Link} from "react-router-dom";
+import React, { useState } from "react";
+import { RouteComponentProps, Link } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
-import { Layout, List, Typography } from "antd";
-import { ListingCard, PageSkeleton } from "../../lib/components";
+import { Affix, Layout, List, Typography } from "antd";
+import { ListingCard } from "../../lib/components";
 import { LISTINGS } from "../../lib/graphql/queries";
 import {
   Listings as ListingsData,
   ListingsVariables,
 } from "../../lib/graphql/queries/Listings/__generated__/Listings";
 import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from "./components";
+import { ErrorBanner } from "../../lib/components";
 
 interface MatchParams {
   location: string;
@@ -20,23 +26,35 @@ const { Title, Paragraph, Text } = Typography;
 const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
   const { data, error, loading } = useQuery<ListingsData, ListingsVariables>(
     LISTINGS,
     {
       variables: {
         location: match.params.location,
-        filter: ListingsFilter.PRICE_LOW_TO_HIGH,
+        filter,
         limit: PAGE_LIMIT,
-        page: 1,
+        page: page,
       },
     }
   );
 
   if (loading) {
-    
+    return (
+      <Content className="listings">
+        <ListingsSkeleton />
+      </Content>
+    );
   }
 
   if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="We either couldn't find anything matching your search or have encountered an error. If you're searching for a unique location, try searching again with more common key words"/>
+        <ListingsSkeleton />
+      </Content>
+    );
   }
 
   const listings = data ? data.listings : null;
@@ -50,20 +68,31 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
 
   const listingsSectionElement =
     listings && listings.result.length ? (
-      <List
-        grid={{
-          gutter: 8,
-          xs: 1,
-          sm: 2,
-          lg: 4,
-        }}
-        dataSource={listings.result}
-        renderItem={(listing) => (
-          <List.Item>
-            <ListingCard listing={listing} />
-          </List.Item>
-        )}
-      />
+      <div>
+        <Affix offsetTop={64}>
+          <ListingsPagination
+            total={listings.total}
+            page={page}
+            limit={PAGE_LIMIT}
+            setPage={setPage}
+          />
+          <ListingsFilters filter={filter} setFilter={setFilter} />
+        </Affix>
+        <List
+          grid={{
+            gutter: 8,
+            xs: 1,
+            sm: 2,
+            lg: 4,
+          }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </div>
     ) : (
       <div>
         <Paragraph>
@@ -71,7 +100,8 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
           <Text mark> "{listingsRegion}"</Text>
         </Paragraph>
         <Paragraph>
-          Be the first person to creat a <Link to="/host">listing in this area</Link>!
+          Be the first person to creat a{" "}
+          <Link to="/host">listing in this area</Link>!
         </Paragraph>
       </div>
     );
